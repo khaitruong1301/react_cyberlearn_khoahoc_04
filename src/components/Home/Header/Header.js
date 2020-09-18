ea02fcab07f9606072d0146712cb0d23c5131d64
khai <khaitruong1301@gmail.com>
2020-09-19 04:46:11 +0700

    'video_3_huongDanSuDungTheNavLink'

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,20 +1,23 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom'
+import './Header.css'
 export default function Header() {
+
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <a className="navbar-brand" href="/">Cyberlearn</a>
+            <NavLink className="navbar-brand" to="/">Cyberlearn</NavLink>
             <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation" />
             <div className="collapse navbar-collapse" id="collapsibleNavId">
                 <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link" href="/home">Home </a>
+                        <NavLink activeClassName="activeNavItem" activeStyle={{fontWeight:'bold'}} className="nav-link" to="/home">Home </NavLink>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/about">About</a>
+                        <NavLink activeClassName="activeNavItem" activeStyle={{fontWeight:'bold'}} className="nav-link" to="/about">About</NavLink>
                     </li>
                     <li className="nav-item dropdown">
-                    <a className="nav-link" href="/contact">Contact</a>
+                    <NavLink activeClassName="activeNavItem" activeStyle={{fontWeight:'bold'}} className="nav-link" to="/contact">Contact</NavLink>
                     </li>
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
