912e57dd02ec0c04cf8cd319670d21bdc02a18c3
khai <khaitruong1301@gmail.com>
2020-10-28 17:31:04 +0700

    'video_30_CYBERBUG(JIRA)_formik_yup_chucNangLogin_p1''

--- src/pages/CyberBugs/LoginCyberBugs/LoginCyberBugs.js
+++ src/pages/CyberBugs/LoginCyberBugs/LoginCyberBugs.js
@@ -1,26 +1,36 @@
 import React from 'react'
 import { Button, Input } from 'antd';
 import { UserOutlined, LockOutlined, FacebookOutlined,TwitterOutlined } from '@ant-design/icons';
+import {withFormik,Formik} from 'formik'
+import * as Yup from 'yup';
 
+ function LoginCyberBugs(props) {
 
-export default function LoginCyberBugs(props) {
-
+    const {
+        values,
+        touched,
+        errors,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+      } = props;
 
     return (
-        <form className="container" style={{ height: window.innerHeight }} >
+        <form onSubmit={handleSubmit} className="container" style={{ height: window.innerHeight }} >
             <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: window.innerHeight }} >
-                <h3 className="text-center" style={{fontWeight:300,fontSize:35}}>Login CyberBugs</h3>
-
+                <h3 className="text-center" style={{fontWeight:300,fontSize:35}}> </h3>
                 <div className="d-flex mt-3" >
-                    <Input style={{ width: '100%', minWidth: 300 }} name="email" size="large" placeholder="email" prefix={<UserOutlined />} />
-                </div>
+                    <Input onChange={handleChange} style={{ width: '100%', minWidth: 300 }} name="email" size="large" placeholder="email" prefix={<UserOutlined />} />     
+                </div> 
+                <div className="text-danger">{errors.email}</div>
                 <div className="d-flex mt-3">
-                    <Input style={{ width: '100%', minWidth: 300 }} type="password" name="email" size="large" placeholder="password" prefix={<LockOutlined />} />
+                    <Input onChange={handleChange} style={{ width: '100%', minWidth: 300 }} type="password" name="password" size="large" placeholder="password" prefix={<LockOutlined />} />
                 </div>
+                <div className="text-danger">{errors.password}</div>
 
-                <Button size="large" style={{ minWidth: 300, backgroundColor: 'rgb(102,117,223)', color: '#fff' }} className="mt-5">Login</Button>
+                <Button htmlType="submit" size="large" style={{ minWidth: 300, backgroundColor: 'rgb(102,117,223)', color: '#fff' }} className="mt-5">Login</Button>
 
-
+ 
                 <div className="social mt-3 d-flex">
                     <Button style={{backgroundColor:'rgb(59,89,152)'}}  shape="circle" size={"large"}>
                         <span className="font-weight-bold" style={{color:'#fff'}} >F</span>
@@ -34,3 +44,24 @@ export default function LoginCyberBugs(props) {
         </form>
     )
 }
+
+const LoginCyberBugsWithFormik =  withFormik({
+    mapPropsToValues: () => ({
+        email: '', 
+        password:''
+    }),
+    validationSchema: Yup.object().shape({
+        email:Yup.string().required('Email is required!').email('email is invalid!'),
+        password:Yup.string().min(6,'password must have min 6 characters').max(32,'password  have max 32 characters')
+
+    }),
+    handleSubmit: (values, { setSubmitting }) => {
+        console.log(values);
+    },
+    displayName: 'Login CyberBugs',
+  })(LoginCyberBugs);
+
+
+
+
+export default LoginCyberBugsWithFormik;
--- src/templates/HomeTemplate/UserLoginTemplate.js
+++ src/templates/HomeTemplate/UserLoginTemplate.js
@@ -16,7 +16,7 @@ export const UserLoginTemplate = (props) => {
     return <Route {...restRoute} render={(propsRoute)=>{
         return <>
             <Layout>
-                <Sider width={window.innerWidth/2} style={{height:window.innerHeight,backgroundImage:'url(https://picsum.photos/500)'}}>
+                <Sider width={window.innerWidth/2} style={{height:window.innerHeight,backgroundImage:'url(https://picsum.photos/2000)',backgroundSize:'100%'}}>
                   
                 </Sider>
                 <Content>
