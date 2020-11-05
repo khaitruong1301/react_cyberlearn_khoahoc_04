0ef84de0f8a8dca381fa3e80ff3550bc58ad0f86
khai <khaitruong1301@gmail.com>
2020-11-05 16:42:26 +0700

    'video35_cyberbugs_createProject_p1'

--- src/components/Cyberbugs/MenuCyberbugs.js
+++ src/components/Cyberbugs/MenuCyberbugs.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function MenuCyberbugs() {
     return (
@@ -14,29 +15,29 @@ export default function MenuCyberbugs() {
             </div>
             <div className="control">
                 <div>
-                    <i className="fa fa-credit-card" />
-                    <span>Cyber Board</span>
+                    <i className="fa fa-credit-card mr-1" />
+                    <NavLink className="text-dark" activeStyle={{color:'blue'}} to="/cyberbugs" activeClassName="active font-weight-bold text-primary" >Cyber Board</NavLink>
                 </div>
                 <div>
-                    <i className="fa fa-cog" />
-                    <span>Project Settings</span>
+                    <i className="fa fa-cog mr-1" /> 
+                    <NavLink className="text-dark"  activeStyle={{color:'blue'}} to='/createproject' activeClassName="active font-weight-bold  text-primary" >Create project</NavLink>
                 </div>
             </div>
             <div className="feature">
                 <div>
-                    <i className="fa fa-truck" />
+                    <i className="fa fa-truck mr-1" />
                     <span>Releases</span>
                 </div>
                 <div>
-                    <i className="fa fa-equals" />
+                    <i className="fa fa-equals mr-1" />
                     <span>Issues and filters</span>
                 </div>
                 <div>
-                    <i className="fa fa-paste" />
+                    <i className="fa fa-paste mr-1" />
                     <span>Pages</span>
                 </div>
                 <div>
-                    <i className="fa fa-location-arrow" />
+                    <i className="fa fa-location-arrow mr-1" />
                     <span>Reports</span>
                 </div>
                 <div>
--- src/pages/CyberBugs/CreateProject/CreateProject.js
+++ src/pages/CyberBugs/CreateProject/CreateProject.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import { Editor } from '@tinymce/tinymce-react';
 
 export default function CreateProject(props) {
+
+
+    const handleEditorChange = (content, editor) => {
+        console.log('Content was updated:', content);
+        console.log('Content was updated:', editor);
+    }
+
+
     return (
-        <div>
-            Tạo project
+        <div className="container m-5">
+            <h3>CreateProject</h3>
+            <form className="container">
+                <div className="form-group">
+                    <p>Name</p>
+                    <input className="form-control" name="projectName" />
+                </div>
+                <div className="form-group">
+                    <p>Description</p>
+                    <Editor
+                        name="description"
+                        initialValue=""
+                        init={{
+                            height: 500,
+                            menubar: false,
+                            plugins: [
+                                'advlist autolink lists link image charmap print preview anchor',
+                                'searchreplace visualblocks code fullscreen',
+                                'insertdatetime media table paste code help wordcount'
+                            ],
+                            toolbar:
+                                'undo redo | formatselect | bold italic backcolor | \
+                                alignleft aligncenter alignright alignjustify | \
+                                bullist numlist outdent indent | removeformat | help'
+                        }}
+                        onEditorChange={handleEditorChange}
+                    />
+                </div>
+                <div className="form-group">
+                    <select name="categoryId" className="form-control">
+                        <option>Software</option>
+                        <option>Web</option>
+                        <option>App</option>
+                    </select>
+                </div>
+                <button className="btn btn-outline-primary" type="submit">Create project</button>
+            </form>
         </div>
     )
 }
