124755e555b9b7547bf39eba429e134b9a48d2e7
Administrator <khaitruong1301@gmail.com>
2020-12-23 16:46:54 +0700

    'dp'

--- src/components/Forms/FormCreateTask/FormCreateTask.js
+++ src/components/Forms/FormCreateTask/FormCreateTask.js
@@ -1,6 +1,6 @@
 import { Editor } from '@tinymce/tinymce-react'
-import React from 'react'
-import { Select, Radio } from 'antd';
+import React, { useState } from 'react'
+import { Select, Radio,Slider } from 'antd';
 const { Option } = Select;
 
 const children = [];
@@ -11,6 +11,11 @@ for (let i = 10; i < 36; i++) {
 export default function FormCreateTask(props) {
 
     const [size, setSize] = React.useState('default');
+
+    const [timeTracking,setTimetracking] = useState({
+        timeTrackingSpent:0,
+        timeTrackingRemaining:0
+    })
 
     const handleEditorChange = (content, editor) => {
 
@@ -54,6 +59,7 @@ export default function FormCreateTask(props) {
                     <div className="col-6">
                     <p>Assignees</p>
                     <Select
+                            
                             mode="multiple"
                             size={size}
                             options={[{value:'a12',label:'b12'},{value:'a12',label:'b12'},{value:'a12',label:'b12'}]}
@@ -63,8 +69,46 @@ export default function FormCreateTask(props) {
                             style={{ width: '100%' }}
                         >
                             {children}
-      </Select>
+                    </Select>
+                    <div className="row mt-3">
+                        <div className="col-12">
+                            <p>Original Estimate</p>
+                            <input type="number" min="0" name="originalEstimate" defaultValue="0" className="form-control" height="30"/>
+                        </div>
                     </div>
+
+                    </div>
+                    <div className="col-6">
+                         <p>Time tracking</p>
+
+                        <Slider defaultValue={30} value={timeTracking.timeTrackingSpent} max={Number(timeTracking.timeTrackingSpent) + Number(timeTracking.timeTrackingRemaining)} />
+                        <div className="row">
+                            <div className="col-6 text-left font-weight-bold">{timeTracking.timeTrackingSpent}h logged</div>
+                            <div className="col-6 text-right font-weight-bold">{timeTracking.timeTrackingRemaining}h remaining</div>
+                        </div>
+                        <div className="row" style={{marginTop:5}}>
+                            <div className="col-6">
+                                <p>Time spent</p>
+                                <input type="number" defaultValue="0" min="0" className="form-control" name="timeTrackingSpent" onChange={(e)=>{
+                                    setTimetracking({
+                                        ...timeTracking,
+                                        timeTrackingSpent:e.target.value
+                                    })
+                                }}/>
+                            </div>
+                            <div className="col-6">
+                                <p>Time remaining</p>
+                                <input type="number" defaultValue="0" min="0" className="form-control" name="timeTrackingRemaining"  onChange={(e)=>{
+                                    setTimetracking({
+                                        ...timeTracking,
+                                        timeTrackingRemaining:e.target.value
+                                    })
+                                }}/>
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
             <div className="form-group">
