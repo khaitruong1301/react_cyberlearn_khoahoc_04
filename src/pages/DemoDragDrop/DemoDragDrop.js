14a6f705b9af354f7fdc7b1e8ea87438c19d8adf
Administrator <khaitruong1301@gmail.com>
2021-01-15 16:29:46 +0700

    'video69_cyberbugs_editask_p10'

--- src/pages/DemoDragDrop/DemoDragDrop.js
+++ src/pages/DemoDragDrop/DemoDragDrop.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const defaultValue = [
     { id: 1, taskName: 'Task 1' },
@@ -12,17 +12,37 @@ const defaultValue = [
 export default function DemoDragDrop(props) {
 
     const [taskList, setTaskList] = useState(defaultValue);
-
+    const tagDrag = useRef({})
 
     const handleDragStart = (e, task, index) => {
-        // console.log('tag',e.target);
-        // console.log('task',task);
-        // console.log('index',index);
+        console.log(e.target);
+        console.log(task);
+        //Lưu lại giá trị của task đang drag
+        tagDrag.current = task;
     }
 
 
-    const handleDragOver = (e) => {
-        // console.log('targertOver',e.target)
+    const handleDragEnter = (e,taskDragEnter,index) => {
+        // console.log('dragEnterTag',e.target)
+        // console.log('targertOver',task)
+        // console.log('index',index)
+
+        let taskListUpdate = [...taskList];
+        //Láy ra index thằng đang kéo
+        let indexDragTag = taskListUpdate.findIndex(task => task.id === tagDrag.current.id);
+        //Lấy ra index thằng bị kéo qua 
+        let indexDragEnter = taskListUpdate.findIndex(task => task.id === taskDragEnter.id);
+
+        //Bién6 chứa giá trị thằng đang kéo
+        let temp = taskListUpdate[indexDragTag];
+        //Lấy giá trị tại vi trí đang kéo gán = thằng kéo qua
+        taskListUpdate[indexDragTag] = taskListUpdate[indexDragEnter];
+        //Lấy thằng kéo qua gán = đang keo
+        taskListUpdate[indexDragEnter] = temp;
+        
+
+        setTaskList(taskListUpdate);
+
      
     }
 
@@ -31,19 +51,18 @@ export default function DemoDragDrop(props) {
        
     }
     const handleDrop = (e) => {
-        console.log('drop', e.target);
+        // console.log('drop', e.target);
     }
     return (
         <div className="container">
-            <div className="text-center display-4" onDragOver={handleDragOver}>Task list</div>
+            <div className="text-center display-4">Task list</div>
             <div className="row">
                 <div className="col-2"></div>
                 <div className="bg-dark p-5 col-4">
                     {taskList.map((task, index) => {
                         return <div
                             onDragStart={(e) => { handleDragStart(e, task, index) }}
-                            onDragEnter={handleDragOver}
-                            onDragEnd={(e) => { handleDragEnd(e) }}
+                            onDragEnter={(e)=>{ handleDragEnter(e,task,index)}}
                             draggable="true"
                             key={index}
                             className="bg-success text-white m-1 p-3">
@@ -51,15 +70,15 @@ export default function DemoDragDrop(props) {
                         </div>
                     })}
                 </div>
-                <div className="col-2 bg-primary" style={{ height: 500 }}  onDragOver={(e)=>{
-                       e.stopPropagation();
-                       e.preventDefault();
-                }} onDrop={(e) => { handleDrop(e) }}>
-                    dsadsasda
-                </div>
+                <div className="col-2 bg-primary" style={{ height: 500 }} ></div>
 
             </div>
 
         </div>
     )
 }
+
+// onDragOver={(e)=>{
+//     e.stopPropagation();
+//     e.preventDefault();
+// }}
