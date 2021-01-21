30eb555fbeba1562f41f85c13f614611aa2d1662
Administrator <khaitruong1301@gmail.com>
2021-01-21 18:25:41 +0700

    'video_73_deployweb(end)'

---
+++ build/precache-manifest.f51237d4e02dee560d0315963899ba5c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4fa46aec27e75a135ef81117183fc54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c2d4286aba6fa97dc5a",
+    "url": "/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "b26c6e78496e8ed3832a",
+    "url": "/static/css/main.4db6b658.chunk.css"
+  },
+  {
+    "revision": "0c2d4286aba6fa97dc5a",
+    "url": "/static/js/2.1fac01c3.chunk.js"
+  },
+  {
+    "revision": "8c082d53fa248d63e1aede82934af5da",
+    "url": "/static/js/2.1fac01c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b26c6e78496e8ed3832a",
+    "url": "/static/js/main.22502e20.chunk.js"
+  },
+  {
+    "revision": "ef624c4d4e219d7fb43e",
+    "url": "/static/js/runtime-main.c5285391.js"
+  },
+  {
+    "revision": "9b2b410b3894da9da9e86fea5832dafa",
+    "url": "/static/media/bg.9b2b410b.png"
+  },
+  {
+    "revision": "e96d3ef90db23e6aca4d9b13481ee1b4",
+    "url": "/static/media/download (1).e96d3ef9.jfif"
+  },
+  {
+    "revision": "1c8cee9f3c0c94be03f180afa6181f47",
+    "url": "/static/media/download.1c8cee9f.jfif"
+  },
+  {
+    "revision": "504b535c80c8095184901c10e561233b",
+    "url": "/static/media/loading.504b535c.gif"
+  }
+]);
